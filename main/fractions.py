b5faa50b023094e4fdef4968e0cddacfe4c6f169
Eric Shaw <shaweric01@gmail.com>
2023-04-02 01:36:12 -0400

    redo

--- main/fractions.py
+++ main/fractions.py
@@ -1,23 +1,7 @@
 from dataclasses import dataclass
-from math import gcd
 
 
-@dataclass(repr=False)
+@dataclass
 class Fraction:
     numerator: int
     denominator: int
-
-    def __post_init__(self):
-        greatest_common_divisor = gcd(self.denominator, self.numerator)
-        self.denominator = self.denominator // greatest_common_divisor
-        self.numerator = self.numerator // greatest_common_divisor
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.numerator}/{self.denominator})"
-
-    def __add__(self, other: "Fraction") -> "Fraction":
-        new_dominator = self.denominator * other.denominator
-        new_numerator = (
-            self.numerator * other.denominator + other.numerator * self.denominator
-        )
-        return Fraction(new_numerator, new_dominator)
--- tests/test_fractions.py
+++ tests/test_fractions.py
@@ -1,41 +1,31 @@
 from main.fractions import Fraction
 
+# TODO write notes and then make better tests
 
-class TestFractions:
-    def test_equality(self):
-        assert Fraction(1, 2) == Fraction(1, 2)
+"""
+notes:
 
-    def test_repr(self):
-        assert repr(Fraction(1, 2)) == "Fraction(1/2)"
+0 + 0
+0 + 1
+2 + 0
 
-    def test_reduces_numerator_divisible_by_denominator(self):
-        assert repr(Fraction(3, 9)) == "Fraction(1/3)"
+1 + 2
+-1 + 2
+neg plus neg
 
-    def test_reduces_by_greatest_common_divisor(self):
-        assert repr(Fraction(4, 6)) == "Fraction(2/3)"
+same denom
+1/5 + 1/5
 
-    def test_add_with_same_numerator(self):
-        first = Fraction(1, 4)
-        second = Fraction(2, 4)
-        expected = Fraction(3, 4)
-        assert first + second == expected
+negatives
+-1/5 + 1/5 = 0/1
 
-    def test_add_with_different_denominator_simple_multiple(self):
-        first = Fraction(1, 4)
-        second = Fraction(1, 2)
-        expected = Fraction(3, 4)
-        assert first + second == expected
+multiples denom
+1/2 + 1/4 = 3/4
 
-    def test_add_with_different_denominator_no_common_multiple(self):
-        # 1/3 + 1/4 = 7/12
-        first = Fraction(1, 4)
-        second = Fraction(1, 3)
-        expected = Fraction(7, 12)
-        assert first + second == expected
 
-    def test_add_with_common_multiple(self):
-        # 5/6 + 3/4 = 10/12 + 9/12 = 19/12
-        first = Fraction(5, 6)
-        second = Fraction(3, 4)
-        expected = Fraction(19, 12)
-        assert first + second == expected
+"""
+
+
+class TestFraction:
+    def test_fraction_equals_itself(self):
+        assert Fraction(2, 3) == Fraction(2, 3)
