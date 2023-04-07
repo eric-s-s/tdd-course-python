f8f9a359a4ff17ce547cb2a0c7755a25efeae3e3
Eric Shaw <eric.shaw@datarobot.com>
2023-04-07 16:08:10 -0400

    moved classes to main

---
+++ main/point_of_sale.py
@@ -0,0 +1,11 @@
+class Display:
+    def get_latest(self):
+        return ""
+
+
+class PointOfSaleSystem:
+    def __init__(self, display: Display):
+        pass
+
+    def on_barcode(self, barcode: str):
+        pass
--- tests/test_point_of_sale.py
+++ tests/test_point_of_sale.py
@@ -1,14 +1,4 @@
-class Display:
-    def get_latest(self):
-        return ""
-
-
-class PointOfSaleSystem:
-    def __init__(self, display: Display):
-        pass
-
-    def on_barcode(self, barcode: str):
-        pass
+from main.point_of_sale import Display, PointOfSaleSystem
 
 
 def test_empty_barcode():
