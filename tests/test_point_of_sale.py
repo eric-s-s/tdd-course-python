56931effc8bd9208706fd19532ef2be8da23cc52
Eric Shaw <eric.shaw@datarobot.com>
2023-04-07 16:04:49 -0400

    base setup for PoS

--- tests/test_point_of_sale.py
+++ tests/test_point_of_sale.py
@@ -1,2 +1,22 @@
-def test_true():
-    assert True
+class Display:
+    def get_latest(self):
+        return ""
+
+
+class PointOfSaleSystem:
+    def __init__(self, display: Display):
+        pass
+
+    def on_barcode(self, barcode: str):
+        pass
+
+
+def test_empty_barcode():
+    display = Display()
+
+    system = PointOfSaleSystem(display)
+
+    system.on_barcode(barcode="")
+
+    result = display.get_latest()
+    assert result == ""
