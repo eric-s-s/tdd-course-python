27cfc7790d80a2f86c99b686366abd569b7f7eda
Eric Shaw <shaweric01@gmail.com>
2024-01-06 17:46:55 -0500

    test cleanup

--- tests/test_point_of_sale.py
+++ tests/test_point_of_sale.py
@@ -1,16 +1,12 @@
+import random
+import string
 from typing import Dict
 
 import pytest
 
-from main.point_of_sale import (
-    AbstractPriceLookup,
-    BarCode,
-    BarCodeError,
-    Display,
-    PointOfSaleSystem,
-    PriceNotFoundError,
-    Price,
-)
+from main.point_of_sale import (AbstractPriceLookup, BarCode, BarCodeError,
+                                Display, PointOfSaleSystem, Price,
+                                PriceNotFoundError)
 
 
 class FakePriceLookup(AbstractPriceLookup):
@@ -28,28 +24,9 @@ def display():
     return Display()
 
 
-@pytest.fixture()
-def one_twenty_five_barcode():
-    return BarCode("2345678901")
-
-
-@pytest.fixture()
-def two_fifty_barcode():
-    return BarCode("0987654321")
-
-
-@pytest.fixture()
-def lookup(one_twenty_five_barcode, two_fifty_barcode):
-    barcode_to_price = {
-        one_twenty_five_barcode: Price(1.25),
-        two_fifty_barcode: Price(2.50),
-    }
-    return FakePriceLookup(barcode_to_price)
-
-
-@pytest.fixture()
-def system(lookup, display):
-    return PointOfSaleSystem(display, lookup)
+def get_random_barcode():
+    code_digits = "".join(random.choice(string.digits) for _ in range(10))
+    return BarCode(code_digits)
 
 
 class TestPrice:
@@ -126,88 +103,118 @@ class TestBarcode:
 
 
 class TestPointOfSaleScanSingleItem:
-    @pytest.fixture(params=["$1.25", "$2.50"], ids=["one twenty five", "two fifty"])
-    def barcode_and_expected_string(
-        self, request, one_twenty_five_barcode, two_fifty_barcode
-    ):
-        if request.param == "$1.25":
-            yield one_twenty_five_barcode, request.param
-        else:
-            yield two_fifty_barcode, request.param
-
-    def test_empty_barcode(self, system, display):
-        system.on_barcode(barcode_string="")
+    @pytest.mark.parametrize(
+        "bad_barcode", ["", "bad code"], ids=["empty", "malformed"]
+    )
+    def test_empty_barcode(self, display, bad_barcode):
+        system = PointOfSaleSystem(display, FakePriceLookup({}))
+        system.on_barcode(barcode_string=bad_barcode)
 
         result = display.get_latest()
         assert result == "Bad barcode. Rescan"
 
-    def test_bad_barcode(self, system, display):
-        system.on_barcode(barcode_string="abc123")
-
-        assert display.get_latest() == "Bad barcode. Rescan"
-
-    def test_no_price_data(self, system, display, lookup):
-        missing_barcode_str = "1234509876"
+    def test_no_price_data(self, display):
+        lookup = FakePriceLookup({get_random_barcode(): Price(123)})
         with pytest.raises(PriceNotFoundError):
-            lookup.get_price(BarCode(missing_barcode_str))
+            lookup.get_price(get_random_barcode())
 
+        system = PointOfSaleSystem(display, lookup)
+
+        missing_barcode_str = get_random_barcode().to_string()
         system.on_barcode(missing_barcode_str)
 
         assert display.get_latest() == f"Item not found: {missing_barcode_str}."
 
-    def test_lookup_found(self, barcode_and_expected_string, display, system):
-        barcode, expected = barcode_and_expected_string
+    @pytest.mark.parametrize(
+        "price, expected", [(Price(1234.5), "$1,234.50"), (Price(2.34), "$2.34")]
+    )
+    def test_lookup_found(self, display, price, expected):
+        barcode = get_random_barcode()
+        lookup = FakePriceLookup({get_random_barcode(): Price(498574), barcode: price})
+        system = PointOfSaleSystem(display, lookup)
+
         system.on_barcode(barcode.to_string())
 
         assert display.get_latest() == expected
 
-    def test_lookup_found_with_whitespace(
-        self, barcode_and_expected_string, display, system
-    ):
-        barcode, expected = barcode_and_expected_string
-        system.on_barcode(f"  {barcode.to_string()}  ")
+    @pytest.mark.parametrize("extra_character", list(" \r\n\t"))
+    def test_lookup_found_with_extra_character(self, display, extra_character):
+        barcode = get_random_barcode()
+        price = Price(12)
+        lookup = FakePriceLookup({get_random_barcode(): Price(498574), barcode: price})
+        system = PointOfSaleSystem(display, lookup)
 
-        assert display.get_latest() == expected
-
-    @pytest.mark.parametrize("return_character", list("\r\n\t"))
-    def test_lookup_with_return_characters(
-        self, return_character, display, system, two_fifty_barcode
-    ):
-        barcode_str = (
-            f"{return_character}{two_fifty_barcode.to_string()}{return_character}"
+        system.on_barcode(
+            f"{extra_character*2}{barcode.to_string()}{extra_character*2}"
         )
 
+        assert display.get_latest() == "$12.00"
+
+    def test_lookup_with_all_removed_characters(self, display):
+        barcode = get_random_barcode()
+        price = Price(3.21)
+        lookup = FakePriceLookup({get_random_barcode(): Price(498574), barcode: price})
+        system = PointOfSaleSystem(display, lookup)
+        barcode_str = f" \r \t \n {barcode.to_string()} \r \t \n "
+
         system.on_barcode(barcode_str)
 
-        assert display.get_latest() == "$2.50"
-
-    def test_lookup_with_all_removed_characters(
-        self, display, system, two_fifty_barcode
-    ):
-        barcode_str = f" \r \t \n {two_fifty_barcode.to_string()} \r \t \n "
-
-        system.on_barcode(barcode_str)
-
-        assert display.get_latest() == "$2.50"
+        assert display.get_latest() == "$3.21"
 
 
 class TestPointOfSaleOnTotal:
-    def test_no_items_scanned(self, system, display):
+    def test_no_items_scanned(self, display):
+        system = PointOfSaleSystem(display, FakePriceLookup({}))
         system.on_total()
 
         assert display.get_latest() == "No items scanned. No total."
 
-    def test_one_item_scanned(self, system, display, two_fifty_barcode):
-        system.on_barcode(two_fifty_barcode.to_string())
+    def test_one_item_scanned(self, display):
+        barcode = get_random_barcode()
+        lookup = FakePriceLookup({barcode: Price(23.45)})
+        system = PointOfSaleSystem(display, lookup)
+
+        system.on_barcode(barcode.to_string())
         system.on_total()
 
-        assert display.get_latest() == "Total: $2.50"
+        assert display.get_latest() == "Total: $23.45"
 
-    def test_two_items_scanned(
-        self, system, display, one_twenty_five_barcode, two_fifty_barcode
-    ):
-        system.on_barcode(two_fifty_barcode.to_string())
-        system.on_barcode(one_twenty_five_barcode.to_string())
+    def test_two_items_scanned(self, display):
+        three_fifty = get_random_barcode()
+        two_fifteen = get_random_barcode()
+        lookup = FakePriceLookup({three_fifty: Price(3.5), two_fifteen: Price(2.15)})
+        system = PointOfSaleSystem(display, lookup)
+
+        system.on_barcode(three_fifty.to_string())
+        system.on_barcode(two_fifteen.to_string())
         system.on_total()
 
-        assert display.get_latest() == "Total: $3.75"
+        assert display.get_latest() == "Total: $5.65"
+
+    def test_two_items_scanned_and_missing_bar_code(self, display):
+        prices = [Price(12.34), Price(2348.7)]
+        barcodes = [get_random_barcode(), get_random_barcode()]
+        lookup = FakePriceLookup(dict(zip(barcodes, prices)))
+        system = PointOfSaleSystem(display, lookup)
+
+        system.on_barcode(barcodes[0].to_string())
+        system.on_barcode(get_random_barcode().to_string())
+        system.on_barcode(barcodes[1].to_string())
+        system.on_total()
+
+        expected = f"Total: $2,361.04"
+        assert display.get_latest() == expected
+
+    def test_two_items_scanned_and_bad(self, display):
+        prices = [Price(234.3), Price(1.23)]
+        barcodes = [get_random_barcode(), get_random_barcode()]
+        lookup = FakePriceLookup(dict(zip(barcodes, prices)))
+        system = PointOfSaleSystem(display, lookup)
+
+        system.on_barcode(barcodes[0].to_string())
+        system.on_barcode("oooops")
+        system.on_barcode(barcodes[1].to_string())
+        system.on_total()
+
+        expected = f"Total: $235.53"
+        assert display.get_latest() == expected
